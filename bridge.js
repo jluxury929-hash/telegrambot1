a687eea80a4e38a1e2714fc350e4a3b7053c2c1e
Josh Platt <jluxury929@gmail.com>
2026-02-06 16:03:37 -0500

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -2,7 +2,7 @@ class BridgeManager {
     constructor() { this.page = null; this.cursor = null; this.isAuto = false; }
     init(page, cursor) { this.page = page; this.cursor = cursor; }
     get() {
-        if (!this.page || this.page.isClosed()) throw new Error("BRIDGE_LOST: Launch engine first.");
+        if (!this.page || this.page.isClosed()) throw new Error("BRIDGE_LOST: Open browser first.");
         return { page: this.page, cursor: this.cursor };
     }
 }
