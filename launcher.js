ac62bfd7f7baf3d681ac697a355d881d7fa2d9c5
Josh Platt <jluxury929@gmail.com>
2026-02-06 16:05:59 -0500

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -10,25 +10,35 @@ async function startEngine() {
     const browser = await puppeteer.launch({
         headless: false,
         executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", 
-        args: ['--start-maximized', '--disable-blink-features=AutomationControlled']
+        args: ['--start-maximized', '--disable-dev-shm-usage', '--no-sandbox']
     });
+
     const page = (await browser.pages())[0];
     const cursor = createCursor(page);
-    await page.goto('https://pocketoption.com/en/login/', { waitUntil: 'networkidle2' });
     
+    // Inject the Fast-Action Core
     const inject = async () => {
         await page.evaluate(() => {
-            window.pocket = {
-                click: (dir) => {
+            window.pocketHFT = {
+                execute: (dir) => {
                     const btn = document.querySelector(dir === 'up' ? '.btn-call' : '.btn-put');
-                    if (btn) btn.click();
+                    if (btn) {
+                        // Sub-millisecond physical event dispatch
+                        btn.dispatchEvent(new MouseEvent('mousedown', {bubbles: true}));
+                        btn.dispatchEvent(new MouseEvent('mouseup', {bubbles: true}));
+                        return "EXECUTED";
+                    }
+                    return "MISSING";
                 }
             };
         });
     };
+
     page.on('framenavigated', inject);
+    await page.goto('https://pocketoption.com/en/login/', { waitUntil: 'networkidle2' });
     await inject();
     bridge.init(page, cursor);
     return page;
 }
+
 module.exports = { startEngine };
