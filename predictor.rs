4d683dd13f9530d4f9b85e9f127fbe77e4a316c0
Josh Platt <jluxury929@gmail.com>
2026-02-06 08:03:57 -0500

    Update and rename predictor.rs to xxxxx

--- predictor.rs
+++
@@ -1,9 +0,0 @@
-pub enum Signal { Call, Put, Neutral }
-pub struct AIPredictor;
-
-impl AIPredictor {
-    pub fn get_prediction(prices: &[f64]) -> (Signal, f64) {
-        // High-profit logic here
-        (Signal::Call, 95.0)
-    }
-}
